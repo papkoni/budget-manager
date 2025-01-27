01081700ed584f115b3fd1ece8a2fe9fc9c79625
Nikita <papkonikita.mcht@gmail.com>
2025-01-27 18:00:23 +0300

    start to implement

--- server/Microservices/UserService/UserService.Application/Services/UserService.cs
+++ server/Microservices/UserService/UserService.Application/Services/UserService.cs
@@ -9,47 +9,47 @@ public class UserService
 
     private readonly IPasswordHasher _passwordHasher;
     
-    public async Task<(string, string)> Register(string name, string password, string email)
-    {
-
-        var findUser = await _userRepository.GetByEmail(email);
-        if (findUser != null)
-        {
-            throw new Exception("User already exists");
-        }
-
-        var hashedPassword = _passwordHasher.Generate(password);
-            
-
-        var user = new UserModel(Guid.NewGuid(), name, hashedPassword, email);
-        var (accessToken, refreshToken) = _jwtProvider.Generate(user);
-        var token = RefreshToken.Create(Guid.NewGuid(), refreshToken, DateTime.Now);
-        user.RefreshTokenId = token.Id;
-        await _refreshTokensRepository.Add(token);
-        await _usersRepository.Add(user);
-
-        return (accessToken, refreshToken );
-    }
+    // public async Task<(string, string)> Register(string name, string password, string email)
+    // {
+    //
+    //     var findUser = await _userRepository.GetByEmail(email);
+    //     if (findUser != null)
+    //     {
+    //         throw new Exception("User already exists");
+    //     }
+    //
+    //     var hashedPassword = _passwordHasher.Generate(password);
+    //         
+    //
+    //     var user = new UserModel(Guid.NewGuid(), name, hashedPassword, email);
+    //     var (accessToken, refreshToken) = _jwtProvider.Generate(user);
+    //     var token = RefreshToken.Create(Guid.NewGuid(), refreshToken, DateTime.Now);
+    //     user.RefreshTokenId = token.Id;
+    //     await _refreshTokensRepository.Add(token);
+    //     await _usersRepository.Add(user);
+    //
+    //     return (accessToken, refreshToken );
+    // }
     
     
     
-    public async Task<(string, string, UserModel?)> Login(string email, string password)
-    {
-        var findUser = await _userRepository.GetByEmail(email);
-
-        if (findUser == null) { throw new Exception(); }
-
-
-        var result = _passwordHasher.Verify(password, findUser.PasswordHash);
-
-        if (result == false)
-        {
-            throw new Exception("Failed to login");
-        }
-
-        var (accessToken, refreshToken) = _jwtProvider.Generate(findUser);
-        await _refreshTokensRepository.UpdateToken(findUser.RefreshTokenId, refreshToken);
-        return (accessToken, refreshToken, findUser);
-
-    }
+    // public async Task<(string, string, UserModel?)> Login(string email, string password)
+    // {
+    //     var findUser = await _userRepository.GetByEmail(email);
+    //
+    //     if (findUser == null) { throw new Exception(); }
+    //
+    //
+    //     var result = _passwordHasher.Verify(password, findUser.PasswordHash);
+    //
+    //     if (result == false)
+    //     {
+    //         throw new Exception("Failed to login");
+    //     }
+    //
+    //     var (accessToken, refreshToken) = _jwtProvider.Generate(findUser);
+    //     await _refreshTokensRepository.UpdateToken(findUser.RefreshTokenId, refreshToken);
+    //     return (accessToken, refreshToken, findUser);
+    //
+    // }
 }
--- server/Microservices/UserService/UserService.Infrastructure/Authentication/JwtProvider.cs
+++ server/Microservices/UserService/UserService.Infrastructure/Authentication/JwtProvider.cs
@@ -5,105 +5,106 @@ using UserService.Application.Models;
 
 namespace UserService.Infrastructure;
 
- public class JwtProvider : IJwtProvider
-    {
-        private readonly JwtOptions _options;
-
-        public JwtProvider(IOptions<JwtOptions> options)
-        {
-            _options = options.Value;
-        }
-
-        public (string accessToken, string refreshToken) Generate(UserModel user)
-        {
-            // Генерация Access токена
-            var accessToken = GenerateToken(user, _options.AccessTokenExpiresMinutes);
-
-            // Генерация Refresh токена
-            var refreshToken = GenerateToken(user, _options.RefreshTokenExpiresDays, isRefreshToken: true);
-
-            return (accessToken, refreshToken);
-        }
-
-        public string GenerateAccess(UserModel user)
-        {
-            // Генерация Access токена
-            var accessToken = GenerateToken(user, _options.AccessTokenExpiresMinutes);
-
-            // Генерация Refresh токена
-
-            return accessToken;
-        }
-
-
-        private string GenerateToken(UserModel user, double expiresIn, bool isRefreshToken = false)
-        {
-            var claims = new List<Claim>
-            {
-                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-                new Claim(ClaimTypes.Name, user.Name),
-                new Claim(ClaimTypes.Email, user.Email),
-            };
-
-            if (!isRefreshToken)
-            {
-                claims.Add(new Claim(ClaimTypes.Role, user.Role));
-            }
-            else
-            {
-                claims.Add(new Claim("isRefreshToken", "true"));
-
-            }
-
-            var signingCredentials = new SigningCredentials(
-                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey)),
-                SecurityAlgorithms.HmacSha256
-            );
-
-            var token = new JwtSecurityToken(
-                claims: claims,
-                expires: DateTime.UtcNow.AddMinutes(expiresIn),
-                signingCredentials: signingCredentials
-            );
-
-            return new JwtSecurityTokenHandler().WriteToken(token);
-        }
-
-        public bool ValidateRefreshToken(string refreshToken)
-        {
-            try
-            {
-                var tokenHandler = new JwtSecurityTokenHandler();
-                var key = Encoding.UTF8.GetBytes(_options.SecretKey);
-
-                var validationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = false,
-                    ValidateAudience = false,
-                    ValidateLifetime = true, 
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(key),
-                    ClockSkew = TimeSpan.Zero 
-                };
-
-                var principal = tokenHandler.ValidateToken(refreshToken, validationParameters, out SecurityToken validatedToken);
-
-                var isRefreshTokenClaim = principal.Claims.FirstOrDefault(c => c.Type == "isRefreshToken");
-                if (isRefreshTokenClaim == null && isRefreshTokenClaim.Value != "true")
-                {
-                    throw new SecurityTokenException("Invalid refresh token.");
-                }
-
-                return true;
-            }
-            
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Token validation failed: {ex.Message}");
-                return false;
-            }
-        }
-
-        
-
+  public class JwtProvider {
+                           //: IJwtProvider
+ //    {
+ //        private readonly JwtOptions _options;
+ //
+ //        public JwtProvider(IOptions<JwtOptions> options)
+ //        {
+ //            _options = options.Value;
+ //        }
+ //
+ //        public (string accessToken, string refreshToken) Generate(UserModel user)
+ //        {
+ //            // Генерация Access токена
+ //            var accessToken = GenerateToken(user, _options.AccessTokenExpiresMinutes);
+ //
+ //            // Генерация Refresh токена
+ //            var refreshToken = GenerateToken(user, _options.RefreshTokenExpiresDays, isRefreshToken: true);
+ //
+ //            return (accessToken, refreshToken);
+ //        }
+ //
+ //        public string GenerateAccess(UserModel user)
+ //        {
+ //            // Генерация Access токена
+ //            var accessToken = GenerateToken(user, _options.AccessTokenExpiresMinutes);
+ //
+ //            // Генерация Refresh токена
+ //
+ //            return accessToken;
+ //        }
+ //
+ //
+ //        private string GenerateToken(UserModel user, double expiresIn, bool isRefreshToken = false)
+ //        {
+ //            var claims = new List<Claim>
+ //            {
+ //                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+ //                new Claim(ClaimTypes.Name, user.Name),
+ //                new Claim(ClaimTypes.Email, user.Email),
+ //            };
+ //
+ //            if (!isRefreshToken)
+ //            {
+ //                claims.Add(new Claim(ClaimTypes.Role, user.Role));
+ //            }
+ //            else
+ //            {
+ //                claims.Add(new Claim("isRefreshToken", "true"));
+ //
+ //            }
+ //
+ //            var signingCredentials = new SigningCredentials(
+ //                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey)),
+ //                SecurityAlgorithms.HmacSha256
+ //            );
+ //
+ //            var token = new JwtSecurityToken(
+ //                claims: claims,
+ //                expires: DateTime.UtcNow.AddMinutes(expiresIn),
+ //                signingCredentials: signingCredentials
+ //            );
+ //
+ //            return new JwtSecurityTokenHandler().WriteToken(token);
+ //        }
+ //
+ //        public bool ValidateRefreshToken(string refreshToken)
+ //        {
+ //            try
+ //            {
+ //                var tokenHandler = new JwtSecurityTokenHandler();
+ //                var key = Encoding.UTF8.GetBytes(_options.SecretKey);
+ //
+ //                var validationParameters = new TokenValidationParameters
+ //                {
+ //                    ValidateIssuer = false,
+ //                    ValidateAudience = false,
+ //                    ValidateLifetime = true, 
+ //                    ValidateIssuerSigningKey = true,
+ //                    IssuerSigningKey = new SymmetricSecurityKey(key),
+ //                    ClockSkew = TimeSpan.Zero 
+ //                };
+ //
+ //                var principal = tokenHandler.ValidateToken(refreshToken, validationParameters, out SecurityToken validatedToken);
+ //
+ //                var isRefreshTokenClaim = principal.Claims.FirstOrDefault(c => c.Type == "isRefreshToken");
+ //                if (isRefreshTokenClaim == null && isRefreshTokenClaim.Value != "true")
+ //                {
+ //                    throw new SecurityTokenException("Invalid refresh token.");
+ //                }
+ //
+ //                return true;
+ //            }
+ //            
+ //            catch (Exception ex)
+ //            {
+ //                Console.WriteLine($"Token validation failed: {ex.Message}");
+ //                return false;
+ //            }
+ //        }
+ //
+ //        
+ //
     }
